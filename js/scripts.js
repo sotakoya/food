a40dbde0237840e07ebdfd1dfd1eab2efd855adc
sotakoya <166267567+sotakoya@users.noreply.github.com>
2024-05-27 23:17:32 +0000

    menu upd 1

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const triggerTabList = document.querySelectorAll('#myTab a')
+triggerTabList.forEach(triggerEl => {
+  const tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', event => {
+    event.preventDefault()
+    tabTrigger.show()
+  })
+})
